5fb5457cf50d9e1cb0b1c05026d9d2a6b604e283
B Eric Davis <feralmonkey@gmail.com>
2019-02-11 05:58:42 -0500

    resize textarea for edit and add forms

--- app/forms.py
+++ app/forms.py
@@ -4,15 +4,15 @@ from flask_wtf import FlaskForm
 from app.models import Question
 
 class AddQuestionForm(FlaskForm):
-    question = TextAreaField('Question', validators=[DataRequired()])
-    answer = TextAreaField('Answer', validators=[DataRequired()])
+    question = TextAreaField('Question', render_kw={"rows": 2, "cols": 80}, validators=[DataRequired()])
+    answer = TextAreaField('Answer', render_kw={"rows": 2, "cols": 80}, validators=[DataRequired()])
     topic = StringField('Topic', validators=[DataRequired()])
     subtopic = StringField('Subtopic')
     submit = SubmitField('Submit')
 
 class EditQuestionForm(FlaskForm):
-    question = TextAreaField('Question', validators=[DataRequired()])
-    answer = TextAreaField('Answer', validators=[DataRequired()])
+    question = TextAreaField('Question', render_kw={"rows": 2, "cols": 80}, validators=[DataRequired()])
+    answer = TextAreaField('Answer', render_kw={"rows": 2, "cols": 80}, validators=[DataRequired()])
     topic = StringField('Topic', validators=[DataRequired()])
     subtopic = StringField('Subtopic')
     submit = SubmitField('Submit')
